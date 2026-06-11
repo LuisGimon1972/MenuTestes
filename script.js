aedfc9c45d9f570a5ce80082fb6cf475c5515b92
Luis Manuel <luismanuel@SGBR.intra>
2026-06-11 17:46:17 -0300

    Ajustes varios

--- script.js
+++ script.js
@@ -1,7 +1,75 @@
 async function executar(cmd) {
+  
   const log = document.getElementById('log')
+ let valorcmd;
 
-  log.innerText += '\n⏳ Executando: ' + cmd + '\n'
+switch (cmd) {
+  case "login":
+    valorcmd = "Autenticação do Sistema";
+    break;
+
+  case "navegacao":
+    valorcmd = "Navegação do Sistema";
+    break;
+
+  case "pessoas":
+    valorcmd = "Cadastro de Pessoas";
+    break;
+
+  case "usuarios":
+    valorcmd = "Cadastro de Usuários";
+    break;
+
+  case "perfil":
+    valorcmd = "Gestão de Perfis";
+    break;
+
+  case "funcionarios":
+    valorcmd = "Cadastro de Funcionários";
+    break;
+
+  case "produtos":
+    valorcmd = "Cadastro de Produtos";
+    break;
+
+  case "especies":
+    valorcmd = "Cadastro de Espécies";
+    break;
+
+  case "cotacao":
+    valorcmd = "Consulta de Cotação de moedad";
+    break;
+
+  case "grupos":
+    valorcmd = "Cadastro de Grupos";
+    break;
+
+  case "subgrupos":
+    valorcmd = "Cadastro de Subgrupos";
+    break;
+
+  case "marcas":
+    valorcmd = "Cadastro de Marcas";
+    break;
+
+  case "validacaopessoas":
+    valorcmd = "Validação de Pessoas";
+    break;
+
+  case "validacaoprodutos":
+    valorcmd = "Validação de Produtos";
+    break;
+
+  case "todos":
+    valorcmd = "Execução completa dos testes";
+    break;
+
+  default:
+    valorcmd = "Comando não encontrado";
+    break;
+}
+
+  log.innerText += '\n⏳ Executando: ' + valorcmd + '\n'
 
   try {
     const res = await fetch('http://localhost:3000/executar', {
